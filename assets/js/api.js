aef405b5fc66cec671e14051c452c41968907c94
basavarajakj2000 <basavarajakj06@gmail.com>
2023-05-07 20:55:48 +0530

    Ui is completed

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,3 +5,23 @@
  */
 
 'use strict';
+
+/**
+ * Fetch data from server
+ * @param {string } url API url [required]
+ * @param {*} successCallback Success callback [required] 
+ * @param {*} errorCallback Error callback [required]
+ */
+
+export async function fetchData(url, successCallback, errorCallback) {
+  
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  } else {
+    const error = await response.json();
+    errorCallback && errorCallback(error);
+  }
+}
--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@
  */
 const addEventOnElement = function ($elements, eventType, callback) {
   for (const $item of $elements) {
-    $item.addEventListeners(eventType, callback)
+    $item.addEventListener(eventType, callback)
   }
   
 }
@@ -44,4 +44,49 @@ $searchToggler.addEventListener('click', function () {
   isExpanded = isExpanded ? false : true;
   this.setAttribute('aria-expanded', isExpanded);
   $searchField.focus()
+})
+
+
+/**
+ * Tab navigation
+ */
+
+const /** {NodeList} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const /** {NodeList} */ $tabPanels = document.querySelectorAll('[data-tab-panel]');
+
+let /** {NodeElement} */[$lastActiveTabBtn] = $tabBtns;
+let /** {NodeElement} */[$lastActiveTabPanel] = $tabPanels;
+
+
+addEventOnElement($tabBtns, 'click', function () {
+  $lastActiveTabBtn.setAttribute('aria-selected', 'false')
+  $lastActiveTabPanel.setAttribute('hidden', "");
+
+  this.setAttribute('aria-selected', 'true');
+  const /** {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+
+  $currentTabPanel.removeAttribute("hidden");
+
+  $lastActiveTabBtn = this;
+  $lastActiveTabPanel = $currentTabPanel;
+});
+
+
+/**
+ * Keyboard accessibility for tab buttons
+ */
+
+addEventOnElement($tabBtns, "keydown", function (e) {
+  const /** {NodeElement} */ $nextElement = this.nextElementSibling;
+  const /** {NodeElemet} */ $previousElement = this.previousElementSibling;
+
+  if (e.key === 'ArrowRight' && $nextElement) {
+    this.setAttribute("tabindex", "-1");
+    $nextElement.setAttribute("tabindex", '0');
+    $nextElement.focus();
+  } else if (e.key === 'ArrowLeft' && $previousElement) {
+    this.setAttribute('tabindex', '-1');
+		$previousElement.setAttribute('tabindex', '0');
+		$previousElement.focus();
+  }
 })
