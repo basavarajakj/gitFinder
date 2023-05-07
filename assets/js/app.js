db97190809a8e06a59c8568bc1099ce5f061197a
basavarajakj2000 <basavarajakj06@gmail.com>
2023-05-07 16:41:43 +0530

    mobile view header section

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,43 @@
  */
 
 'use strict';
+
+/**
+ * Add eventlistener on multiple elements
+ * @param {NodeList} $element 
+ * @param {String} eventType 
+ * @param {Function} callback 
+ */
+const addEventOnElement = function ($elements, eventType, callback) {
+  for (const $item of $elements) {
+    $item.addEventListeners(eventType, callback)
+  }
+  
+}
+
+
+/**
+ * HEADER scroll state
+ */
+
+const /** {NodeElement} */ $header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', function () {
+  $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active')
+})
+
+
+/**
+ * Search toggle
+ */
+
+const /** {NodeElement} */ $searchToggler = document.querySelector('[data-search-toggler]');
+const /** {NodeElement} */ $searchField = document.querySelector('[data-search-field]');
+let /** {Boolean} */ isExpanded = false;
+
+$searchToggler.addEventListener('click', function () {
+  $header.classList.toggle('search-active');
+  isExpanded = isExpanded ? false : true;
+  this.setAttribute('aria-expanded', isExpanded);
+  $searchField.focus()
+})
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,3 +6,29 @@
 
 
 'use strict';
+
+const /** {NodeElement} */ $HTML = document.documentElement;
+const /** {Boolean} */ isDark = window.matchMedia("(prefers-color-schema: dark").matches;
+
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "light"
+}
+
+let /** {Boolean} */ isPressed = false;
+
+const changeTheme = function() {
+  isPressed = isPressed ? false : true;
+  this.setAttribute('aria-pressed', isPressed);
+
+  $HTML.setAttribute("data-theme", ($HTML.dataset.theme === 'light') ? "dark" : "light");
+
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+window.addEventListener("load", function () {
+  const /** {NodeElement} */ $themeBtn = this.document.querySelector('[data-theme-btn]');
+
+  $themeBtn.addEventListener('click', changeTheme);
+})
